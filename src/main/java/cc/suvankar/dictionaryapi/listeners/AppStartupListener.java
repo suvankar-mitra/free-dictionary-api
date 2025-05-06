21e75c17d50d186c7e5c522f160736079f572d0a
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2025-05-07 01:03:43 +0530

    Adding parse all files at app startup

---
+++ src/main/java/cc/suvankar/dictionaryapi/listeners/AppStartupListener.java
@@ -0,0 +1,70 @@
+package cc.suvankar.dictionaryapi.listeners;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import cc.suvankar.dictionaryapi.services.XmlProcessor;
+
+@Component
+@Profile("!test")
+public class AppStartupListener {
+
+    private static final Logger LOG = LoggerFactory
+            .getLogger(AppStartupListener.class);
+
+    @Autowired
+    private XmlProcessor xmlProcessor;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        LOG.info("Application is ready to serve requests. Parsing files...");
+        try {
+            // Automatically parse all files at startup
+            parseAllFiles();
+        } catch (Exception e) {
+            LOG.error("Error during startup parsing: {}", e.getMessage());
+        }
+    }
+
+    private void parseAllFiles() {
+        Path directory = Paths.get("src/main/resources/GCIDE");
+
+        int numberOfThreads = Runtime.getRuntime().availableProcessors();
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+
+        try (Stream<Path> files = Files.list(directory)) {
+            files.filter(Files::isRegularFile)
+                    .map(Path::getFileName)
+                    .forEach(filePath -> {
+                        String fileName = filePath.toString();
+                        if (fileName.startsWith("CIDE") && fileName.endsWith(".xml")) {
+                            LOG.info("Parsing file {}", fileName);
+                            executorService.execute(() -> {
+                                try {
+                                    xmlProcessor.processAndPersistXml(fileName);
+                                } catch (Exception e) {
+                                    LOG.error("Error processing file {}: {}", fileName, e.getMessage());
+                                }
+                            });
+                        }
+                    });
+            executorService.shutdown();
+            executorService.awaitTermination(Long.MAX_VALUE, java.util.concurrent.TimeUnit.NANOSECONDS);
+            LOG.info("Parsing completed");
+        } catch (Exception e) {
+            LOG.error("Fatal error {}", e.getMessage());
+        }
+
+    }
+}
